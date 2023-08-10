5a65993c5601072f652cde2eced1658aedd3e600
AndriyMerzlikin <lastheroofme@gmail.com>
2023-08-10 16:33:11 +0300

    first commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
 
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+}
+
+const currentTime = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+
+if (currentTime) {
+  player
+    .setCurrentTime(currentTime.seconds)
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+
+        default:
+          break;
+      }
+    });
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+
+const input = document.querySelector('.feedback-form input');
+
+const textarea = document.querySelector('.feedback-form textarea');
+
+input.addEventListener('input', throttle(onInput, 500));
+textarea.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+onCheckStorage();
+
+function onInput() {
+  const data = {
+    email: input.value,
+    message: textarea.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+}
+
+function onCheckStorage() {
+  const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (savedData) {
+    input.value = savedData.email;
+    textarea.value = savedData.message;
+  }
+}
