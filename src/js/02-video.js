5c482ccf71faac49f79706a2fcba74f65fd90b36
AndriyMerzlikin <lastheroofme@gmail.com>
2023-08-12 20:31:21 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const throttle = require('lodash.throttle');
 
@@ -8,7 +9,7 @@ const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(onPlay, 250));
+player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay(data) {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
